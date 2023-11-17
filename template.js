e2805e1aeac12201e11403b52fe94d7b045eddd2
jelil-info <oyekanmijelili@gmail.com>
2023-11-17 17:43:30 +0000

    tem

--- template.js
+++ template.js
@@ -3,10 +3,9 @@ export default ({markup, css}) => {
       <html lang="en">
         <head>
           <meta charset="utf-8">
-          <meta name="viewport" content="initial-scale=1, width=device-width" />
+          <meta name="viewport" content="width=500, initial-scale=1" />
 
-          <meta name="Keywords" content="Suok, The medium to sell all things">
-          <meta name="author" content="Oyekanmi Oyetunji">
+          
           
           <title>Online Market</title>
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400">
